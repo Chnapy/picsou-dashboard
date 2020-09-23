3bd645b4c5601a1b06d6762da56e3eb3b2892115
Chnapy <richardhaddad@hotmail.fr>
2020-09-24 01:19:58 +0200

    remove functions test (test-integrations do the job)

--- packages/fire-functions/functions/src/index.test.ts
+++ packages/fire-functions/functions/src/index.test.ts
@@ -1,18 +1,8 @@
-import { requestStockHistory } from './index';
-import { fbTester } from './fb-tester';
-import { getStockHistoryValuesTestData } from '@picsou/shared';
 
 
-describe('routes', () => {
+describe('placeholder', () => {
 
-    it('requestStockHistory', async () => {
-
-        const request = fbTester.wrap(requestStockHistory);
-
-        const { paramsQuery, expectedData } = getStockHistoryValuesTestData();
-
-        const data = await request(paramsQuery);
-
-        expect(data).toEqual(expectedData);
+    it('placeholder', async () => {
+        expect(true).toBe(true);
     });
 });
