0e0b64607722f3e2f63e702f2dc9672d347fc0b9
Richard Haddad <richardhaddad@hotmail.fr>
2020-10-10 20:46:32 +0200

    Setup deployment (#2)
    * pass to node 12
    * change monorepo structure
    * fix functions deployment (workaround)
    * add github action deploy

--- packages/functions/.eslintrc.js
+++ packages/functions/.eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
-
     },
     ignorePatterns: ['jest.config.js'],
     plugins: ["@typescript-eslint", "jest"],
